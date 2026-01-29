6847721dff6ea628666c80632f98fc6f94cb5ee9
sharoukhan214-del <sharoukhan214@gmail.com>
2026-01-29 08:03:15 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// مفاتيح فايربيس الخاصة بك
+const firebaseConfig = {
+    apiKey: "AIzaSyBp6XDnQj80f1B1c5hW",
+    databaseURL: "https://snay3y-pro-default-rtdb.firebaseio.com",
+    projectId: "snay3y-pro"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+// بيانات الإدارة العليا (قبطان السيستم)
+const ADMIN_DATA = {
+    phone: "01020744825",
+    pass: "Mohamed214@",
+    instapay_phone: "01020744825",
+    max_debt: 1000
+};
